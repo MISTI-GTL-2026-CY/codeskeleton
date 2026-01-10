4d4f37c3eb33ebbce1524cce2767ee33f02a2d82
Ilia Nechaev <lelikk2002@gmail.com>
2026-01-10 16:50:43 +0200

    fix: capitalize Node from rclpy.node

--- src/blank_package/blank_package/blank_node.py
+++ src/blank_package/blank_package/blank_node.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import os
 import rclpy
-from rclpy.node import node
+from rclpy.node import Node
 
 # Fill in something for msg type imports
 # from duckietown_msgs.msg import SOMETHING
 # from std_msgs.msg import SOMETHING
 
-class SkeletonNode(node):
+class SkeletonNode(Node):
     def __init__(self):
         super().__init__('blank_node')
         #Create publishers and subscribers in init, use callback
